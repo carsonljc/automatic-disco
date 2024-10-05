fb13dc06b7511e3f70f54e21c17619ef77b85cf0
Carson <30126272+carsonljc@users.noreply.github.com>
2024-10-05 23:19:11 +0800

    relocate s3 upload

--- start_disco.sh
+++ start_disco.sh
@@ -2,15 +2,13 @@
 
 # Function to display usage
 usage() {
-    echo "Usage: $0 --bucket <BUCKET_NAME> --object <OBJECT_NAME> --prefix <OUTLINE_PREFIX>"
+    echo "Usage: $0 --prefix <OUTLINE_PREFIX>"
     exit 1
 }
 
 # Parse command-line arguments
 while [[ "$#" -gt 0 ]]; do
     case "$1" in
-        --bucket) BUCKET_NAME="$2"; shift ;;
-        --object) OBJECT_NAME="$2"; shift ;;
         --prefix) OUTLINE_PREFIX="$2"; shift ;;
         *) usage ;;
     esac
@@ -18,7 +16,7 @@ while [[ "$#" -gt 0 ]]; do
 done
 
 # Check if all required parameters are provided
-if [[ -z "$BUCKET_NAME" || -z "$OBJECT_NAME" || -z "$OUTLINE_PREFIX" ]]; then
+if [[-z "$OUTLINE_PREFIX" ]]; then
     usage
 fi
 
@@ -38,10 +36,6 @@ curl --insecure -X PUT "$API_URL/access-keys/default"
 # Store the configs
 curl --insecure -X GET "$API_URL/access-keys/default" | jq --arg prefix "$OUTLINE_PREFIX" '{ server: (.accessUrl | split("@")[1] | split(":")[0]), server_port: (.port), password: (.password), method: (.method), prefix: $prefix }' > /tmp/outline.json
 curl --insecure -X GET "$API_URL/access-keys/default" | jq '{"version": 1, "servers": [{ server: (.accessUrl | split("@")[1] | split(":")[0]), server_port: (.port), password: (.password), method: (.method) }]}' > /tmp/shadowsocks.json
-
-# Save configs to S3
-aws s3api put-object --bucket "$BUCKET_NAME" --key "outline/$OBJECT_NAME" --body /tmp/outline.json --acl public-read
-aws s3api put-object --bucket "$BUCKET_NAME" --key "shadowsocks/$OBJECT_NAME" --body /tmp/shadowsocks.json --acl public-read
 
 # Set tcp congestion control
 sudo sh -c 'echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf'
